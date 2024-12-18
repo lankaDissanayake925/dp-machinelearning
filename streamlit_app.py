514c205a16fd0a5b1807c75139e6607992987f85
lankaDissanayake925 <lankadissanayake925@gmail.com>
2024-12-18 09:24:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title('🎈 Machine Learning App')
 
 st.info('This is a machine learning app')
+df=pd.read_csv('https://raw.githubcontent.com/dataprofessor/data/master/penguins_cleaned.csv')
